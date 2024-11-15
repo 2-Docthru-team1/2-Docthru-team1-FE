c9b95c18954dcace99c2eeeb8d202b3312b0e2e3
hyobiin9 <moone1234@kakao.com>
2024-11-15 22:34:44 +0900

    🐛 Recipe list page

---
+++ src/pages/RecipeList.tsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import RecipeCard from '@/components/Card/RecipeCard';
+
+export async function getStaticProps() {
+  const res = await axios.get('http://localhost:3000/mockData.json');
+  const initialData = await res.data;
+  return {
+    props: {
+      initialData,
+    },
+  };
+}
+
+interface RecipeData {
+  title: string;
+  likeCount: number;
+  category: string;
+  images: string[];
+}
+
+interface RecipeListProps {
+  initialData: RecipeData[];
+}
+
+export default function RecipeList({ initialData }: RecipeListProps) {
+  return (
+    <div>
+      {initialData.map((data, index) => (
+        <RecipeCard key={index} data={data} />
+      ))}
+    </div>
+  );
+}
