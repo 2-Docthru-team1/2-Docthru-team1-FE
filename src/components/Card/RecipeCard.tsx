ad1db179130598a7c6c234a06a8458d6e39d5e5b
hyobiin9 <moone1234@kakao.com>
2024-11-15 20:57:14 +0900

    ✨ Feat: 레시피카드 ui 제작
    목데이터 연결 X

--- src/components/Card/RecipeCard.tsx
+++ src/components/Card/RecipeCard.tsx
@@ -1,3 +1,20 @@
+import Image from 'next/image';
+import inactiveHeart from '@/../public/assets/icon_heart_inact_small.png';
+import foodImg from '@/../public/temporaryAssets/Food.svg';
+import styles from './RecipeCard.module.css';
+
 export default function RecipeCard() {
-  return <div>This is Recipe Card Component file</div>;
+  return (
+    <div className={styles.card}>
+      <Image src={foodImg} alt="음식 이미지" />
+      <div className={styles.info}>
+        <p className={styles.category}>TRADITIONAL</p>
+        <p className={styles.food_name}>BIBIMBAP</p>
+        <div className={styles.heart_info}>
+          <Image src={inactiveHeart} alt="하트" />
+          <p className={styles.heart_count}>84</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import RecipeCard from '@/components/Card/RecipeCard';
 import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <>
       <Header>HanCook</Header>
+      <RecipeCard />
     </>
   );
 }
