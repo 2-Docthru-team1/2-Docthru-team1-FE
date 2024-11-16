0242558c124feca0450335837056014cc44c06a9
hyobiin9 <moone1234@kakao.com>
2024-11-16 15:57:53 +0900

    🐛 Fix: prettier 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     screens: {
-      sm: "343px",
-      md: "696px",
-      lg: "1200px",
+      sm: '343px',
+      md: '696px',
+      lg: '1200px'
     },
     colors: {
-      "primary-dark-gray": "#262626",
-      "primary-light-gray": "#f1f2f5",
-      "primary-beige": "#d5bda6",
-      "primary-blue": "#73a8cd",
-      "gray-900": "#111827",
-      "gray-800": "#1f2937",
-      "gray-700": "#374151",
-      "gray-600": "#4b5563",
-      "gray-500": "#6b7280",
-      "gray-400": "#9ca3af",
-      "gray-200": "#e5e7eb",
-      "gray-100": "#f3f4f6",
-      "gray-50": "#f9fafb",
-      "error-red": "#f74747",
+      'primary-dark-gray': '#262626',
+      'primary-light-gray': '#f1f2f5',
+      'primary-beige': '#d5bda6',
+      'primary-blue': '#73a8cd',
+      'gray-900': '#111827',
+      'gray-800': '#1f2937',
+      'gray-700': '#374151',
+      'gray-600': '#4b5563',
+      'gray-500': '#6b7280',
+      'gray-400': '#9ca3af',
+      'gray-200': '#e5e7eb',
+      'gray-100': '#f3f4f6',
+      'gray-50': '#f9fafb',
+      'error-red': '#f74747'
     },
     extend: {
       textColor: {
-        DEFAULT: "#374151",
-      },
-    },
+        DEFAULT: '#374151'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 
 export default config;
