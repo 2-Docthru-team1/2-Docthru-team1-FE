4943a94ff5bb582edd6d772cdd77d37e95b1e5a7
hyobiin9 <moone1234@kakao.com>
2024-11-17 17:42:17 +0900

    🐛 Fix:#20-레시피 리스트 페이지 타이틀 제거

--- src/app/recipeList/page.tsx
+++ src/app/recipeList/page.tsx
@@ -3,7 +3,6 @@ import RecipeListClient from '@/components/ClientWrapper/RecipeListClient';
 export default function RecipePage() {
   return (
     <div>
-      <h1 className="text-center text-3xl font-bold">Recipe List</h1>
       <RecipeListClient />
     </div>
   );
