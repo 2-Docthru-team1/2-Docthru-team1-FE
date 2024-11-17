b0846c3abfecef15a04dfaf8181302d07560a9e0
hyobiin9 <moone1234@kakao.com>
2024-11-17 23:42:01 +0900

    ✨ Feat:#25-레시피 디테일페이지 (component 사용X)

--- src/components/ClientWrapper/RecipeDetailClient.tsx
+++ src/components/ClientWrapper/RecipeDetailClient.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import inactiveHeart from '@/../public/assets/icon_heart_inact_small.png';
 import { fetchRecipe } from '@/api/recipeService';
 import type { RecipeDetailData } from '@/interfaces/recipelistInterface';
 
@@ -26,12 +27,23 @@ export default function RecipeDetailClient() {
   }
 
   return (
-    <div className="flex flex-col items-center mt-[2rem]">
+    <div className="flex flex-col mt-[2rem] w-full items-center">
       <div className="relative w-[120rem] h-[33rem] overflow-hidden">
         {recipe?.images.length > 0 && (
           <Image src={recipe.images[0]} alt="음식 이미지" layout="fill" objectFit="cover" objectPosition="center" />
         )}
       </div>
+      <div className="mt-[2rem] flex flex-col w-[120rem] gap-[1rem]">
+        <p className="font-semibold text-[2rem] leading-[2.387rem] text-gray-800">{recipe.category}</p>
+        <div className="flex w-full gap-[2rem] items-center">
+          <p className="font-semibold text-[3.2rem] leading-[3.819rem] text-gray-700">{recipe.title}</p>
+          <div className="flex gap-[0.4rem] items-center">
+            <Image src={inactiveHeart} alt="하트" width={24} height={24} />
+            <p className="font-medium text-[1.4rem] leading-[1.671rem] text-gray-700">{recipe.likeCount}</p>
+          </div>
+        </div>
+      </div>
+      <div className="border border-gray-200 w-[120rem] mt-[2rem]"></div>
     </div>
   );
 }
