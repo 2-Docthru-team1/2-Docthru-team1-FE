d096dbd1b355a335297beb630df6a26ae6471f5a
hyobiin9 <moone1234@kakao.com>
2024-11-17 21:31:22 +0900

    ✨ Feat:#27-detail text card type 지정해서 ui 적용

--- src/interfaces/cardInterface.ts
+++ src/interfaces/cardInterface.ts
@@ -8,3 +8,18 @@ export interface RecipeData {
   category: string;
   images: string[];
 }
+
+export interface DetailTextCardProps {
+  type: 'ingredient' | 'direction' | 'nutrition' | 'benefit';
+  content: string[] | NutritionContent;
+}
+
+export interface NutritionContent {
+  calories: string;
+  carbs: string;
+  protein: string;
+  fat: string;
+  sugars: string;
+  sodium: string;
+  fiber: string;
+}
