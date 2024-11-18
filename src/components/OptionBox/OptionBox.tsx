b7e6238b33ffdbe3b5d23403c78fc89e82eb00b3
hyobiin9 <moone1234@kakao.com>
2024-11-18 17:59:37 +0900

    ✨ Feat:#38-옵션박스 컴포넌트 스타일 추가

--- src/components/OptionBox/OptionBox.tsx
+++ src/components/OptionBox/OptionBox.tsx
@@ -9,18 +9,24 @@ export default function OptionBox({ type }: OptionBoxProps) {
   const participateButtonText = type === 'ongoing' ? 'Keep Participating' : 'Participate Challenge';
 
   return (
-    <div>
-      <div>
-        <Image src={clock} alt="시계" />
-        <p>Closing on 2024-03-03</p>
-      </div>
-      <div>
-        <Link href="/">
-          <button>See Content</button>
-        </Link>
-        <Link href="/">
-          <button>{participateButtonText}</button>
-        </Link>
+    <div className="w-[28.5rem] h-[17.6rem] rounded-[1.6rem] border-2 border-gray-100 flex items-center justify-center">
+      <div className="w-[25.3] flex flex-col gap-[1.6rem]">
+        <div className="flex items-center justify-center gap-[0.4rem]">
+          <Image src={clock} alt="시계" />
+          <p className="font-normal text-[1.3rem] leading-[1.551rem] text-gray-600">Closing on 2024-03-03</p>
+        </div>
+        <div className="flex flex-col items-center gap-[0.8rem] w-[25.3rem]">
+          <Link href="/" className="w-full">
+            <button className="rounded-[0.8rem] bg-primary-blue w-full h-[4rem] font-bold text-[1.4rem] leading-[2.6rem] text-[#ffffff]">
+              See Content
+            </button>
+          </Link>
+          <Link href="/" className="w-full">
+            <button className="rounded-[0.8rem] bg-primary-beige w-full h-[4rem] font-bold text-[1.4rem] leading-[2.6rem] text-[#ffffff]">
+              {participateButtonText}
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
