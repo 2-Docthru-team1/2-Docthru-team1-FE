3bfcea2fe5395a4e88d6b25b94ab39d3d892af8e
unknown <d653@naver.com>
2024-11-19 15:30:17 +0900

    ✨ Feat:#46 - 작업물 이미지 클릭 시 확대
    모달을 넣는 것과 비슷하게 클릭 시 모달 대신 이미지가 나오도록 설정

--- src/components/Card/WorkCard.tsx
+++ src/components/Card/WorkCard.tsx
@@ -1,5 +1,6 @@
 import { format } from 'date-fns';
 import Image from 'next/image';
+import { useState } from 'react';
 import nextImage from '@/../public/assets/btn_photo_swipe.png';
 import inactiveHeart from '@/../public/assets/icon_heart_inactive_large.png';
 import member from '@/../public/assets/img_profile_member.png';
@@ -13,6 +14,10 @@ export default function WorkCard({ data, user }: WorkDataProps) {
 
   const formattedDate = format(new Date(data.createdAt), 'yy/MM/dd HH:mm');
   const formattedNumber = data.likeCount.toLocaleString();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openImg = () => setIsOpen(true);
+  const closeImg = () => setIsOpen(false);
 
   return (
     <div className="flex flex-col w-[120rem] gap-[1rem]">
@@ -32,14 +37,24 @@ export default function WorkCard({ data, user }: WorkDataProps) {
         </div>
       </div>
       <div className="flex">
-        <div className="mr-[0.3rem] w-[47.6rem] h-[47.6rem] relative">
-          <Image src={food} alt="작업물 이미지" fill className="object-cover" /> {/* src={data.images} */}
+        <div className="mr-[0.3rem] w-[47.6rem] h-[47.9rem] relative cursor-pointer">
+          <Image src={food} alt="작업물 이미지" fill className="object-cover" onClick={openImg} /> {/* src={data.images} */}
         </div>
         <div className="flex items-center">
           <Image src={nextImage} alt="다음 이미지 버튼" />
         </div>
         <p className="text-[1.6rem] font-normal text-gray-800">{data.content}</p>
       </div>
+      {isOpen && (
+        <div className="w-full h-full fixed inset-0 bg-gray-900 bg-opacity-50 z-2" onClick={closeImg}>
+          <div
+            className="releative fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[70.7rem] h-[74.1rem]"
+            onClick={e => e.stopPropagation()}
+          >
+            <Image src={food} alt="작업물 확대 이미지" fill className="object-cover" /> {/* src={data.images} */}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
