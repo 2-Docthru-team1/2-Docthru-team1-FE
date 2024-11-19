1744acd91d54fc61c76f6bd6e07e74438445b81f
Jaewon Kim <magry78@gmail.com>
2024-11-19 15:07:36 +0900

    ✨ Feat:#50-칩 카테고리 구현
    칩 카테고리 만듦.

---
+++ src/components/Chip/ChipCategory.tsx
@@ -0,0 +1,11 @@
+import type { ChipCategoryProps } from '@/interfaces/chipInterface';
+
+export default function ChipCategory({ mediaType }: ChipCategoryProps) {
+  return (
+    <>
+      <div className="flex gap-[1rem] items-center justify-center rounded-[0.8rem] border-[0.1rem] border-solid border-gray-300 bg-white py-[0.5rem] px-[0.7rem]">
+        <p className="text-[1.3rem] font-[500] text-gray-600">{mediaType}</p>
+      </div>
+    </>
+  );
+}
--- src/interfaces/chipInterface.ts
+++ src/interfaces/chipInterface.ts
@@ -1,3 +1,7 @@
 export interface ChipCardProps {
   type: 'ongoing' | 'finished';
 }
+
+export interface ChipCategoryProps {
+  mediaType: 'Youtube' | 'Blog' | 'Recipe Web' | 'Social Media';
+}
