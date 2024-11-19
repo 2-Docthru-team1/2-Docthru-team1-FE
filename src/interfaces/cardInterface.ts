795bdfe0effca141ce5b628a1adec818042ef5b8
hyobiin9 <moone1234@kakao.com>
2024-11-19 14:57:05 +0900

    🐛 Fix:#35-충돌 해결

--- src/interfaces/cardInterface.ts
+++ src/interfaces/cardInterface.ts
@@ -38,3 +38,16 @@ export interface ChallengeParticipantStatusData {
   images: string[];
   role: string;
 }
+
+export interface MonthlyChallengeCardProps {
+  data: MonthlyChallengeCard | null;
+}
+
+export interface MonthlyChallengeCard {
+  id: string;
+  title: string;
+  cuisineType: 'Traditional' | 'School Food' | 'Noodle' | 'BanChan' | 'Dessert';
+  mediaType: 'Youtube' | 'Blog' | 'Recipe Web' | 'Social Media';
+  status: 'ongoing' | 'closed';
+  closingDate: string;
+}
