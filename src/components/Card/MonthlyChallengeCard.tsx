e6b049e443aaf193919125e56650b9efcbe67d5b
Jaewon Kim <magry78@gmail.com>
2024-11-20 10:09:42 +0900

    ✨ Feat:#56-유저 연
    유저 연결 기반 세팅.

--- src/components/Card/MonthlyChallengeCard.tsx
+++ src/components/Card/MonthlyChallengeCard.tsx
@@ -6,22 +6,19 @@ import ChipCard from '@/components/Chip/ChipCard';
 import ChipCategoryCard from '@/components/Chip/ChipCategory';
 import type { MonthlyChallengeCardProps } from '@/interfaces/cardInterface';
 
-export default function MonthlyChallengeCard({ data }: MonthlyChallengeCardProps) {
+export default function MonthlyChallengeCard({ data, user }: MonthlyChallengeCardProps) {
   if (!data) {
     return <div>로딩 중...</div>;
   }
 
-  const { title, mediaType, status, deadline } = data;
-  const formattedClosingDate = new Date(deadline).toISOString().split('T')[0];
+  const { title, mediaType, status, deadline, requestUserId } = data;
+  const formattedDeadline = new Date(deadline).toISOString().split('T')[0];
 
-  // {user.id === data.ownerId && <div>드롭다운</div>}
+  // NOTE 도엽님이 완성하면 kebabToggle 이미지로만 넣어놓은 부분 Component로 바꿀 예정.
+  // NOTE 본인 챌린지만 보이게 설정 - {user.id === requestUserId && <div>드롭다운</div>}
   return (
     <div className="w-[38.4rem] gap-[1rem] rounded-[1.2rem] border-[0.2rem] border-solid border-primary-beige">
-      <div className="">
-        {/* <div className="absolute top-[1rem] right-[1rem] z-10">
-          <Image src={kebabToggle} alt="More Options" />
-        </div> */}
-
+      <div>
         <div className="p-[2.4rem]">
           <div className="flex justify-between items-center">
             <div className="flex items-center gap-[1.2rem]">
@@ -43,7 +40,7 @@ export default function MonthlyChallengeCard({ data }: MonthlyChallengeCardProps
 
           <div className="flex items-center gap-[0.2rem]">
             <Image src={clockIcon} alt="Deadline" />
-            <div className="text-[1.3rem] text-gray-500">Closing on {formattedClosingDate}</div>
+            <div className="text-[1.3rem] text-gray-500">Closing on {formattedDeadline}</div>
           </div>
         </div>
       </div>
