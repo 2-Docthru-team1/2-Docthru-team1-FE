e0581d4065c64b7f6843140b64e876872d4b4187
unknown <d653@naver.com>
2024-11-21 15:26:54 +0900

    🐛 Fix:#71- 피드백 컴포넌트 style & UI
    피드백 컴포넌트 스타일과 전체적인 구성 수정

--- src/components/Card/FeedbackCard.tsx
+++ src/components/Card/FeedbackCard.tsx
@@ -1,9 +1,10 @@
 import { format } from 'date-fns';
 import Image from 'next/image';
-import { useState } from 'react';
+import { type ChangeEvent, useState } from 'react';
 import kebab from '@/../public/assets/icon_kebab_cancel.png';
 import userImg from '@/../public/assets/img_profile_member.png';
 import type { FeedbackCardProps } from '@/interfaces/feedbackInterface';
+import CancelDropdown from '../Dropdown/CancelDropdown';
 
 export default function FeedbackCard({ comments, user }: FeedbackCardProps) {
   if (!comments || comments.length === 0) {
@@ -25,6 +26,10 @@ export default function FeedbackCard({ comments, user }: FeedbackCardProps) {
     setIsDropdownOpen(false);
   };
 
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
+    setEditingContent(e.target.value);
+  };
+
   const handleEdit = (commentId: string) => {
     try {
       // TODO: 추후 댓글 수정 api 연결
@@ -40,7 +45,11 @@ export default function FeedbackCard({ comments, user }: FeedbackCardProps) {
       <ul className="flex-col">
         {comments.map(comment => (
           <li key={comment.id} className="flex-col p-[1.2rem] ">
-            <div className="w-[120rem] bg-gray-50 p-[1rem]">
+            <div
+              className={`w-[120rem] p-[1rem] rounded-[0.8rem] ${
+                editingCommentId === comment.id ? 'bg-primary-white border border-gray-200' : 'bg-gray-50 border-none'
+              }`}
+            >
               <div className="flex justify-between items-center gap-[3rem] mb-[1.2rem]">
                 <div className="flex gap-[1rem] items-center">
                   <div className="w-[3.2rem] h-[3.2rem] relative">
@@ -52,21 +61,38 @@ export default function FeedbackCard({ comments, user }: FeedbackCardProps) {
                   </div>
                 </div>
                 {editingCommentId === comment.id && (
-                  <div>
-                    <button onClick={() => setEditingCommentId('')}>Cancel</button>
-                    <button onClick={() => handleEdit(comment.id)}>Complete</button>
+                  <div className="flex gap-[0.5rem]">
+                    <button
+                      className="w-[8.9rem] h-[3.2rem] rounded-[0.8rem] border border-gray-200 text-[1.4rem] font-semibold text-gray-700"
+                      onClick={() => setEditingCommentId('')}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      className="w-[8.9rem] h-[3.2rem] rounded-[0.8rem] border bg-primary-blue border-gray-200 text-[1.4rem] font-semibold text-primary-white"
+                      onClick={() => handleEdit(comment.id)}
+                    >
+                      Complete
+                    </button>
                   </div>
                 )}
-                {user.id === comment.ownerId && (
-                  <div>
-                    <Image src={kebab} alt="드롭다운 이미지" onClick={() => handleMenuClick} className="cursor-pointer" />
-                    {isDropdownOpen && <button onClick={() => handleEditClick(comment)}>드롭다운</button>}
+                {user.id === comment.ownerId && editingCommentId !== comment.id && (
+                  <div className="flex-col relative">
+                    <Image src={kebab} alt="드롭다운 이미지" onClick={handleMenuClick} className="cursor-pointer" />
+                    <div className="absolute right-0" onClick={() => handleEditClick(comment)}>
+                      {isDropdownOpen && <CancelDropdown onCancel={() => handleEditClick(comment)}>Edit</CancelDropdown>}
+                    </div>
                   </div>
                 )}
               </div>
               {editingCommentId === comment.id ? (
                 <div>
-                  <textarea id="content" value={editingContent} />
+                  <textarea
+                    id="content"
+                    value={editingContent}
+                    onChange={handleChange}
+                    className="border-none w-[117rem] h-[4rem] text-[1.6rem] font-normal text-gray-700 focus: outline-none resize-none"
+                  />
                 </div>
               ) : (
                 <div>
