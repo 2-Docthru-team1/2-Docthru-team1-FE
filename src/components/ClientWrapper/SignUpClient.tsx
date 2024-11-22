62df655a39db65d6e9f477ef40b885dd430ede51
unknown <d653@naver.com>
2024-11-22 13:43:27 +0900

    🐛 Fix:#89- 클라이언트에서만 실행되도록 설정 use client

--- src/components/ClientWrapper/SignUpClient.tsx
+++ src/components/ClientWrapper/SignUpClient.tsx
@@ -1,4 +1,9 @@
-import useSignUpValidate from '../../../hooks/useSignUPValidate';
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import logoImg from '@/../public/assets/img_logo_pc.png';
+import useSignUpValidate from '../../../hooks/useSignUpValidate';
 
 export default function SignUpClient() {
   const { values, errors, validate, handleChange } = useSignUpValidate({
@@ -8,5 +13,19 @@ export default function SignUpClient() {
     passwordConfirmation: ''
   });
 
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <Image src={logoImg} alt="로고 이미지" />
+      </div>
+      <form>
+        {/* 재원님이 만드신 인풋 컴포넌트 삽입 */}
+        <button></button>
+      </form>
+      <div>
+        <p>Do you have an account?</p>
+        <Link href="/signIn">Sign in</Link>
+      </div>
+    </div>
+  );
 }
