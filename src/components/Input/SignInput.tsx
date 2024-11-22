46d40f47dfb676ee69ed26beb516899e0107f27d
Jaewon Kim <magry78@gmail.com>
2024-11-22 11:26:36 +0900

    ✨ Feat:#86-스토리북 생성&css 수정
    스토리북으로 테스트하며 css 수정

--- src/components/Input/SignInput.tsx
+++ src/components/Input/SignInput.tsx
@@ -3,15 +3,15 @@ import type { SignInputProps } from '@/interfaces/signInterface';
 
 export default function SignInput({ type, label, placeholder, ...props }: SignInputProps) {
   return (
-    <div className="w-[51.8rem] mb-[2.4rem]">
-      <label htmlFor={type} className="text-gray-700 font-medium mb-[0.8rem] text-[1.4rem]">
+    <div className="flex flex-col w-[51.8rem] mb-[2.4rem]">
+      <label htmlFor={type} className="text-gray-700 font-medium mb-[0.8rem] text-[1.4rem] leading-[1.6rem]">
         {label}
       </label>
       <input
         id={type}
         type={type}
         placeholder={placeholder}
-        className="w-full bg-primary-white border border-gray-200 rounded-[1.2rem]rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 py-[1.1rem] px-[2rem]"
+        className="w-full bg-primary-white border border-gray-200 rounded-[1.2rem] focus:outline-none focus:ring-1 focus:ring-primary-beige py-[1.1rem] px-[2rem] text-[1.6rem] text-left placeholder-gray-400 text-gray-700"
         {...props}
       />
     </div>
---
+++ src/stories/SignInput.stories.tsx
@@ -0,0 +1,40 @@
+import type { StoryFn } from '@storybook/react';
+import React from 'react';
+import SignInput from '@/components/Input/SignInput';
+import type { SignInputProps } from '@/interfaces/signInterface';
+
+export default {
+  title: 'Components/SignInput',
+  component: SignInput,
+  argTypes: {
+    type: {
+      control: { type: 'select' },
+      options: ['text', 'email', 'password']
+    },
+    label: { control: 'text' },
+    placeholder: { control: 'text' }
+  }
+};
+
+const Template: StoryFn<SignInputProps> = args => <SignInput {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  type: 'text',
+  label: 'Username',
+  placeholder: 'Enter your username'
+};
+
+export const EmailInput = Template.bind({});
+EmailInput.args = {
+  type: 'email',
+  label: 'Email',
+  placeholder: 'Please enter your email'
+};
+
+export const PasswordInput = Template.bind({});
+PasswordInput.args = {
+  type: 'password',
+  label: 'Password',
+  placeholder: 'Enter your password'
+};
