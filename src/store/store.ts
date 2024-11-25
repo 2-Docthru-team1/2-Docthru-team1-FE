41e4f36d8790e7137135bb552c5c5e8515826547
hyobiin9 <moone1234@kakao.com>
2024-11-25 10:49:12 +0900

    🐛 Fix: 배포작업 에러 수정정

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,35 +11,12 @@ interface StoreState {
   isDropdownOpen: boolean;
   setKeyword: (keyword: string) => void;
   setCategory: (category: string) => void;
-<<<<<<< HEAD
   setSelectedSort: (sort: string | null) => void;
   setSelectedView: (view: string) => void;
   setSelectedMedia: (media: string[]) => void;
   setSelectedStatus: (status: string) => void;
   setIsFilterApplied: (isApplied: boolean) => void;
   toggleDropdown: (isOpen: boolean) => void;
-}
-
-const useStore = create<StoreState>(set => ({
-  keyword: '',
-  category: '',
-  selectedSort: null,
-  selectedView: '',
-  selectedMedia: [],
-  selectedStatus: '',
-  isFilterApplied: false,
-  isDropdownOpen: false,
-  setKeyword: keyword => set({ keyword }),
-  setCategory: category => set({ category }),
-  setSelectedSort: sort => set({ selectedSort: sort }),
-  setSelectedView: view => set({ selectedView: view }),
-  setSelectedMedia: media => set({ selectedMedia: media }),
-  setSelectedStatus: status => set({ selectedStatus: status }),
-  setIsFilterApplied: isApplied => set({ isFilterApplied: isApplied }),
-  toggleDropdown: isOpen => set({ isDropdownOpen: isOpen })
-}));
-=======
-
   userStatus: 'loggedOut' | 'normal' | 'admin';
   userId: string | null;
   role: 'normal' | 'admin' | null;
@@ -56,8 +33,20 @@ const useStore = create<StoreState>((set: (partial: Partial<StoreState>) => void
   return {
     keyword: '',
     category: '',
-    setKeyword: (keyword: string) => set({ keyword }),
-    setCategory: (category: string) => set({ category }),
+    selectedSort: null,
+    selectedView: '',
+    selectedMedia: [],
+    selectedStatus: '',
+    isFilterApplied: false,
+    isDropdownOpen: false,
+    setKeyword: keyword => set({ keyword }),
+    setCategory: category => set({ category }),
+    setSelectedSort: sort => set({ selectedSort: sort }),
+    setSelectedView: view => set({ selectedView: view }),
+    setSelectedMedia: media => set({ selectedMedia: media }),
+    setSelectedStatus: status => set({ selectedStatus: status }),
+    setIsFilterApplied: isApplied => set({ isFilterApplied: isApplied }),
+    toggleDropdown: isOpen => set({ isDropdownOpen: isOpen }),
 
     userStatus: initialUserStatus,
     userId: storedUserId || null,
@@ -79,6 +68,5 @@ const useStore = create<StoreState>((set: (partial: Partial<StoreState>) => void
     setUserStatus: (status: 'loggedOut' | 'normal' | 'admin') => set({ userStatus: status })
   };
 });
->>>>>>> 8711591e3d64b6acdd440ee287cb67a4fad12dc9
 
 export default useStore;
