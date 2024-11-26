7df91a7a81cd387788055b0b8f39283cc9d7aeee
hyobiin9 <moone1234@kakao.com>
2024-11-26 21:58:24 +0900

    🐛 Fix:배포 에러 수정-storybook

--- src/stories/WorkCard.stories.tsx
+++ src/stories/WorkCard.stories.tsx
@@ -9,47 +9,12 @@ const data = {
   title: 'My first Bibimbap!',
   createdAt: '2024-11-19T04:44:08.912Z',
   likeCount: 1934,
-  images: ['/temporaryAssets/food.svg'],
+  images: [{ imageUrl: '/temporaryAssets/food.svg' }],
   content: 'I did it for like one hour!! Look at thissss! I finally made it!',
-  owner: {
-    id: 'u123',
-    name: 'Named',
-    role: 'koo-koo'
-  }
+  owner: { id: 'u123', name: 'Named', role: 'koo-koo' }
 };
-
-const user = {
-  id: 'u123',
-  name: 'Named',
-  role: 'Koo-koo'
-};
-
-export default {
-  title: 'Components/WorkCard',
-  component: WorkCard,
-  args: {
-    data,
-    user
-  },
-  parameters: {
-    layout: 'centered'
-  }
-};
-
+const user = { id: 'u123' };
+export default { title: 'Components/WorkCard', component: WorkCard, args: { data, user }, parameters: { layout: 'centered' } };
 const Template: StoryFn<WorkDataProps> = args => <WorkCard {...args} />;
-
 export const Default = Template.bind({});
-Default.args = {
-  data,
-  user
-};
-
-export const Loading = Template.bind({});
-Loading.args = {
-  data: null,
-  user: {
-    id: 'u123',
-    name: 'Loading User',
-    role: 'Loading Role'
-  }
-};
+Default.args = { data, user };
