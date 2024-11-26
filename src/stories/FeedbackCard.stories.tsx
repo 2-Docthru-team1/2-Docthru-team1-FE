775e6070b2c259ff0bbec4aea06ce36fe38cb415
hyobiin9 <moone1234@kakao.com>
2024-11-26 21:53:06 +0900

    🐛 Fix: 배포 에러 수정-storybook

--- src/stories/FeedbackCard.stories.tsx
+++ src/stories/FeedbackCard.stories.tsx
@@ -2,44 +2,19 @@ import type { StoryFn } from '@storybook/react';
 import FeedbackCard from '@/components/Card/FeedbackCard';
 
 const mockComments = [
-  {
-    id: '1',
-    content: 'This is a comment.',
-    createdAt: new Date().toISOString(),
-    ownerId: '123',
-    userName: 'John Doe'
-  },
+  { id: '1', content: 'This is a comment.', createdAt: new Date().toISOString(), ownerId: '123', owner: { name: 'John Doe' } },
   {
     id: '2',
     content: 'Another comment for testing.',
     createdAt: new Date().toISOString(),
     ownerId: '456',
-    userName: 'user2'
+    owner: { name: 'user2' }
   }
 ];
-
-const mockUser = {
-  id: '123',
-  name: 'John Doe',
-  role: 'ko-ko'
-};
-
-export default {
-  title: 'Components/FeedbackCard',
-  component: FeedbackCard,
-  argTypes: {}
-};
-
+const mockUser = { id: '123' };
+export default { title: 'Components/FeedbackCard', component: FeedbackCard, argTypes: {} };
 const Template: StoryFn<typeof FeedbackCard> = args => <FeedbackCard {...args} />;
-
 export const Default = Template.bind({});
-Default.args = {
-  comments: mockComments,
-  user: mockUser
-};
-
+Default.args = { comments: mockComments, user: mockUser };
 export const NoComments = Template.bind({});
-NoComments.args = {
-  comments: [],
-  user: mockUser
-};
+NoComments.args = { comments: [], user: mockUser };
