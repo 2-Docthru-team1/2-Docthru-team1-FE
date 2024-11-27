3d7b6f87c03e7763bed15a563f6c5524b921f0c0
hyobiin9 <moone1234@kakao.com>
2024-11-27 15:59:26 +0900

    ✨ Feat:#144-detail 다듬기

--- src/components/Body/ChallengeApplicationBody.tsx
+++ src/components/Body/ChallengeApplicationBody.tsx
@@ -1,4 +1,7 @@
+import { format } from 'date-fns';
 import type { ChallengeApplicationBodyProps } from '@/interfaces/bodyInterface';
+import ChipCard from '../Chip/ChipCard';
+import ChipStatus from '../Chip/ChipStatus';
 
 enum Status {
   Pending = 'pending',
@@ -17,6 +20,14 @@ enum MediaType {
 }
 
 export default function ChallengeApplicationBody({ data }: ChallengeApplicationBodyProps) {
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    if (isNaN(date.getTime())) {
+      return '';
+    }
+    return format(date, 'yy/MM/dd');
+  };
+
   return (
     <div className="w-[99.6rem]">
       <div className="flex rounded-[0.8rem] bg-primary-beige h-[3.6rem]">
@@ -41,28 +52,30 @@ export default function ChallengeApplicationBody({ data }: ChallengeApplicationB
       </div>
       <div className="mt-[1.6rem] bg-primary-white">
         {data?.map((item, index) => {
-          let statusLabel;
-          let mediaLabel;
+          let statusLabel: 'pend' | 'deny' | 'approve' | 'cancel';
+          let mediaLabel: string;
 
           switch (item.status) {
             case Status.Pending:
-              statusLabel = 'Pending';
+              statusLabel = 'pend';
               break;
             case Status.Finished:
-              statusLabel = 'Approved';
+              statusLabel = 'approve';
               break;
             case Status.Denied:
-              statusLabel = 'Denied';
+              statusLabel = 'deny';
               break;
             case Status.OnGoing:
-              statusLabel = 'Approved';
+              statusLabel = 'approve';
               break;
             case Status.Canceled:
-              statusLabel = 'Canceled';
+              statusLabel = 'cancel';
               break;
             case Status.Aborted:
-              statusLabel = 'Canceled';
+              statusLabel = 'cancel';
               break;
+            default:
+              statusLabel = 'pend';
           }
 
           switch (item.mediaType) {
@@ -78,27 +91,32 @@ export default function ChallengeApplicationBody({ data }: ChallengeApplicationB
             case MediaType.SocialMedia:
               mediaLabel = 'Social Media';
               break;
+            default:
+              mediaLabel = 'Unknown';
           }
 
           return (
-            <div key={index} className="flex items-center justify-between h-[4.8rem] rounded-[0.4rem] border-b border-gray-300">
+            <div key={index} className="flex items-center justify-between h-[4.8rem] border-b border-gray-300">
               <p className="w-[6.8rem] flex items-center justify-center font-normal text-[1.3rem] leading-[1.551rem] text-gray-500">
                 {item.number}
               </p>
               <p className="w-[8.4rem] flex items-center justify-center font-normal text-[1.3rem] leading-[1.551rem] text-gray-500">
                 {mediaLabel}
               </p>
-              <p className="w-[35.8rem] flex items-center justify-center font-normal text-[1.3rem] leading-[1.551rem] text-gray-700">
-                {item.title}
+              <p
+                className="w-[35.8rem] flex items-center font-normal text-[1.3rem] leading-[1.551rem] text-gray-700 
+                  overflow-hidden text-ellipsis whitespace-nowrap max-h-[3rem]"
+              >
+                <span className="block overflow-hidden text-ellipsis overflow-hidden whitespace-normal">{item.title}</span>
               </p>
               <p className="w-[18.3rem] flex items-center justify-center font-normal text-[1.3rem] leading-[1.551rem] text-gray-500">
-                {item.updatedAt}
+                {formatDate(item.updatedAt)}
               </p>
               <p className="w-[18.3rem] flex items-center justify-center font-normal text-[1.3rem] leading-[1.551rem] text-gray-500">
-                {item.deadline}
+                {formatDate(item.deadline)}
               </p>
               <p className="w-[12rem] flex items-center justify-center font-normal text-[1.3rem] leading-[1.551rem] text-gray-500">
-                {statusLabel}
+                <ChipStatus type={statusLabel} />
               </p>
             </div>
           );
--- src/stories/ChallengeApplicationBody.stories.tsx
+++ src/stories/ChallengeApplicationBody.stories.tsx
@@ -13,77 +13,79 @@ export const Default = Template.bind({});
 Default.args = {
   data: [
     {
-      number: 1,
-      mediaType: 'youtube',
-      title: 'Test1',
-      updatedAt: '2024-01-01T00:00:00Z',
-      deadline: '2024-01-15T00:00:00Z',
-      status: 'pending'
-    }
-  ]
-};
-
-export const Approved = Template.bind({});
-Approved.args = {
-  data: [
-    {
-      number: 2,
-      mediaType: 'blog',
-      title: 'Test2',
-      updatedAt: '2024-01-02T00:00:00Z',
-      deadline: '2024-01-16T00:00:00Z',
-      status: 'finished'
-    }
-  ]
-};
-
-export const Canceled = Template.bind({});
-Canceled.args = {
-  data: [
-    {
-      number: 3,
+      number: 10,
       mediaType: 'socialMedia',
       title: 'Test3',
       updatedAt: '2024-01-03T00:00:00Z',
       deadline: '2024-01-17T00:00:00Z',
       status: 'canceled'
-    }
-  ]
-};
-
-export const Denied = Template.bind({});
-Denied.args = {
-  data: [
+    },
     {
-      number: 4,
+      number: 9,
       mediaType: 'recipeWeb',
       title: 'Test4',
       updatedAt: '2024-01-04T00:00:00Z',
       deadline: '2024-01-18T00:00:00Z',
       status: 'denied'
-    }
-  ]
-};
-
-export const OnGoing = Template.bind({});
-OnGoing.args = {
-  data: [
+    },
     {
-      number: 5,
+      number: 8,
       mediaType: 'youtube',
       title: 'Test5',
       updatedAt: '2024-01-05T00:00:00Z',
       deadline: '2024-01-19T00:00:00Z',
       status: 'onGoing'
-    }
-  ]
-};
-
-export const Aborted = Template.bind({});
-Aborted.args = {
-  data: [
+    },
+    {
+      number: 7,
+      mediaType: 'blog',
+      title: 'Test6',
+      updatedAt: '2024-01-06T00:00:00Z',
+      deadline: '2024-01-20T00:00:00Z',
+      status: 'aborted'
+    },
     {
       number: 6,
+      mediaType: 'youtube',
+      title: 'Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1',
+      updatedAt: '2024-01-01T00:00:00Z',
+      deadline: '2024-01-15T00:00:00Z',
+      status: 'pending'
+    },
+    {
+      number: 5,
+      mediaType: 'blog',
+      title: 'Test2Test2Test2Test2Test2Test2Test2Test2Test2Test2Test2Test2Test2Test2',
+      updatedAt: '2024-01-02T00:00:00Z',
+      deadline: '2024-01-16T00:00:00Z',
+      status: 'finished'
+    },
+    {
+      number: 4,
+      mediaType: 'socialMedia',
+      title: 'Test3',
+      updatedAt: '2024-01-03T00:00:00Z',
+      deadline: '2024-01-17T00:00:00Z',
+      status: 'canceled'
+    },
+    {
+      number: 3,
+      mediaType: 'recipeWeb',
+      title: 'Test4',
+      updatedAt: '2024-01-04T00:00:00Z',
+      deadline: '2024-01-18T00:00:00Z',
+      status: 'denied'
+    },
+    {
+      number: 2,
+      mediaType: 'youtube',
+      title: 'Test5',
+      updatedAt: '2024-01-05T00:00:00Z',
+      deadline: '2024-01-19T00:00:00Z',
+      status: 'onGoing'
+    },
+    {
+      number: 1,
       mediaType: 'blog',
       title: 'Test6',
       updatedAt: '2024-01-06T00:00:00Z',
