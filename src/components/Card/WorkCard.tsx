a6774429c67c0ca9a4e14afc68cfee041f4a82bf
unknown <d653@naver.com>
2024-11-28 23:20:47 +0900

    🐛 Fix:#153- 코멘트 반영

--- src/components/Card/WorkCard.tsx
+++ src/components/Card/WorkCard.tsx
@@ -18,9 +18,7 @@ import ImageEnlargeModal from '../Modal/ImageEnlargeModal';
 export default function WorkCard({ data, user }: WorkDataProps) {
   if (!data) return null;
   const router = useRouter();
-  if (!user) {
-    router.push('/signIn');
-  }
+
   enum ImgOrder {
     first = 0,
     second = 1
