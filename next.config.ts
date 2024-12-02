4bc1a46529971703e259186d4f99f17b964535c4
hyobiin9 <moone1234@kakao.com>
2024-12-02 16:49:07 +0900

    ✨ Feat: add docker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
-const nextConfig = {
-  images: { domains: ['hancook-bucket.s3.ap-northeast-2.amazonaws.com'] } /* config options here */
+const nextConfig: NextConfig = {
+  images: { domains: ['hancook-bucket.s3.ap-northeast-2.amazonaws.com'] },
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
