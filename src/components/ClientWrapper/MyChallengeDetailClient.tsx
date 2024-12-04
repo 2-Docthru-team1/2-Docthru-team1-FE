0c335be9e7e812302a5827adb32b4cf0c7df968a
hyobiin9 <moone1234@kakao.com>
2024-12-04 17:32:22 +0900

    ✨ Feat:#220-My challenge detail client s3파일 추가

--- src/components/ClientWrapper/MyChallengeDetailClient.tsx
+++ src/components/ClientWrapper/MyChallengeDetailClient.tsx
@@ -3,17 +3,16 @@
 import Image from 'next/image';
 import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import loading from '@/../public/assets/Message@1x-1.0s-200px-200px.svg';
-import { fetchAdminChallengeDetailNext, fetchAdminChallengeDetailPrev, fetchChallenge_detail } from '@/api/challengeService';
+import { fetchChallenge_detail } from '@/api/challengeService';
 import ChallengeApplicationDetailBody from '@/components/Body/ChallengeApplicationDetailBody';
 import ChallengeApplicationDetailHeader from '@/components/Header/ChallengeApplicationDetailHeader';
 import type { ChallengeApplicationDetailHeaderData } from '@/interfaces/challengeInterface';
-import useStore from '@/store/store';
+
+const S3_BASE_URL = process.env.NEXT_PUBLIC_S3_BASE_URL;
 
 export default function MyChallengeDetailClient() {
   const { id } = useParams();
   const router = useRouter();
-  const { challengeMgmtTotalCount } = useStore();
 
   const [currentData, setCurrentData] = useState<ChallengeApplicationDetailHeaderData | null>(null);
   const [currentPage, setCurrentPage] = useState<number>(1);
@@ -24,30 +23,6 @@ export default function MyChallengeDetailClient() {
       router.push('/');
     }
   }, []);
-
-  const itemsPerPage = 1;
-  const totalPages =
-    currentData && typeof currentData.number === 'number' ? Math.max(1, Math.ceil(challengeMgmtTotalCount / itemsPerPage)) : 1;
-
-  const handlePageChange = async (page: number) => {
-    if (page === currentPage) return;
-
-    setCurrentPage(page);
-
-    let response;
-
-    if (page < currentPage) {
-      response = await fetchAdminChallengeDetailPrev(String(id));
-    } else if (page > currentPage) {
-      response = await fetchAdminChallengeDetailNext(String(id));
-    }
-
-    if (response) {
-      setCurrentData(response);
-    }
-
-    router.push(`/auth/challenge/${response.id}`);
-  };
 
   useEffect(() => {
     const getChallengeDetailData = async () => {
@@ -67,8 +42,8 @@ export default function MyChallengeDetailClient() {
 
   if (!currentData) {
     return (
-      <div className="flex items-center justify-center h-[100vh]">
-        <Image src={loading} alt="로딩" />
+      <div className="flex w-full justify-center items-center min-h-screen">
+        <Image src={`${S3_BASE_URL}/loading.svg`} alt="loading" width={200} height={200} />
       </div>
     );
   }
