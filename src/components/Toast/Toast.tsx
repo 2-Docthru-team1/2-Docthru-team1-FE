af1613ba87408a420e6584bc63873d73707f2c8f
hyobiin9 <moone1234@kakao.com>
2024-12-05 17:57:47 +0900

    ✨ Feat:#238-toast ui 완성

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -6,24 +6,26 @@ import type { ToastProps } from '@/interfaces/toastInterface';
 
 const S3_BASE_URL = process.env.NEXT_PUBLIC_S3_BASE_URL;
 
-export default function ToastComponent({ message, onClose, duration = 10000 }: ToastProps) {
+export default function ToastComponent({ onClose, duration = 10000 }: ToastProps) {
   useEffect(() => {
     const timer = setTimeout(() => {
       onClose();
     }, duration);
 
     return () => clearTimeout(timer);
-  }, [message, duration, onClose]);
+  }, [duration, onClose]);
 
   return (
-    <div>
-      <div>
-        <Image src={`${S3_BASE_URL}/icon_close.svg`} alt="X" width={24} height={24} />
-        <p>Would you like to upload your previous draft?</p>
+    <div className="flex justify-between lg:w-[89rem] md:w-[69.6rem] sm:w-[34.3rem] p-[0.8rem] border border-gray-200 bg-primary-white rounded-[0.8rem]">
+      <div className="flex gap-[0.8rem] items-center">
+        <Image src={`${S3_BASE_URL}/icon_close.svg`} alt="X" width={24} height={24} onClick={onClose} />
+        <p className="font-medium text-[1.4rem] leading-[1.671rem] text-gray-700">
+          Would you like to upload your previous draft?
+        </p>
       </div>
-      <div>
-        <button>Yes</button>
-      </div>
+      <button className="w-[9rem] h-[3.2rem] rounded-[0.8rem] py-[0.2rem] px-[1.6rem] gap-[0.4rem] bg-primary-blue font-semibold text-[1.4rem] leading-[1.671rem] text-primary-white">
+        Yes
+      </button>
     </div>
   );
 }
