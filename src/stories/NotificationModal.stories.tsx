4e9e61c35e00ccadb5d803f49abd19de221a920c
hyobiin9 <moone1234@kakao.com>
2024-12-05 18:28:39 +0900

    🐛 Fix:#238-notification modal storybook 수정

--- src/stories/NotificationModal.stories.tsx
+++ src/stories/NotificationModal.stories.tsx
@@ -21,19 +21,5 @@ EmptyNotifications.args = {
 
 export const MultipleNotifications = Template.bind({});
 MultipleNotifications.args = {
-  notifications: [
-    { content: '새로운 알림입니다.', time: '2024-12-04T06:39:07.703Z' },
-    { content: '업데이트가 완료되었습니다.', time: '2024-12-04T06:45:07.703Z' },
-    { content: '시스템 점검이 예정되어 있습니다.', time: '2024-12-04T08:29:07.703Z' },
-    { content: '새로운 알림입니다.', time: '2024-12-04T06:39:07.703Z' },
-    { content: '업데이트가 완료되었습니다.', time: '2024-12-04T06:45:07.703Z' },
-    {
-      content:
-        '시스템 점검이 예정되어 있습니다.시스템 점검이 예정되어 있습니다.시스템 점검이 예정되어 있습니다.시스템 점검이 예정되어 있습니다.시스템 점검이 예정되어 있습니다.',
-      time: '2024-12-04T08:29:07.703Z'
-    },
-    { content: '새로운 알림입니다.', time: '2024-12-04T06:39:07.703Z' },
-    { content: '업데이트가 완료되었습니다.', time: '2024-12-04T06:45:07.703Z' },
-    { content: '시스템 점검이 예정되어 있습니다.', time: '2024-12-04T08:29:07.703Z' }
-  ]
+  notifications: [{ challengeId: '1', message: '새로운 알림입니다.', createdAt: '2024-12-04T06:39:07.703Z' }]
 };
