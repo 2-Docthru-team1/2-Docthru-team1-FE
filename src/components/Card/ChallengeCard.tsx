d1bdc829aa423eb8765792a65423c793aac91d7c
hyobiin9 <moone1234@kakao.com>
2024-12-08 17:29:04 +0900

    🐛 Fix: 주석 수정

--- src/components/Card/ChallengeCard.tsx
+++ src/components/Card/ChallengeCard.tsx
@@ -19,8 +19,6 @@ export default function ChallengeCard({ type, data, userId, role }: ChallengeCar
     );
   }
 
-  console.log(data, userId, 'userusersuser');
-
   const { id, title, deadline, status, mediaType, requestUser } = data;
 
   const formatDate = (dateString: string) => {
